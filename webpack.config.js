11d689953cfdef72ff3a0d1782179f4b877aed52
Shamako181 <rauljaque181@gmail.com>
2022-03-04 12:41:49 -0300

    Configuracion de webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+//Se comienza a configurar Webpack
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+    entry: './src/index.js',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve:{
+        extensions: ['.js']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js?$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader',
+                }
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin([
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html'
+            }
+        ])
+    ]
+}
